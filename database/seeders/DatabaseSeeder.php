c138f2a2b59133403dfa73cd1ad1b79b481431ce
michailtjhang <michailaja@gmail.com>
2024-08-16 18:05:19 +0700

    feature: add seeder database

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,9 +2,11 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use Carbon\Carbon;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,9 +17,20 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        DB::table('users')->insert([
+            [
+                'id' => '1',
+                'id_user' => 'US001',
+                'name' => 'Admin',
+                'nim' => NULL,
+                'email' => 'admin@gmail.com',
+                'role' => 'admin',
+                'email_verified_at' => NULL,
+                'password' => '$2y$12$eaHr3KoP3gazDWn/d4ZYL.zvjvMSTLUhNyDLEQEsH8OJAP455w3Fu', // Encrypted password
+                'remember_token' => NULL,
+                'created_at' => Carbon::parse('2024-05-07 13:00:18'),
+                'updated_at' => Carbon::parse('2024-05-07 13:00:18'),
+            ],
         ]);
     }
 }
